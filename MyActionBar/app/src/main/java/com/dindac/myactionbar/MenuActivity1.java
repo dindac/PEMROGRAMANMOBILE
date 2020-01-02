971e07dd7b01d0ce08f49478d998cfde3d77eb62
dindac <dindacahya001@gmail.com>
2020-01-02 13:40:31 +0700

    myactionbar

---
+++ MyActionBar/app/src/main/java/com/dindac/myactionbar/MenuActivity1.java
@@ -0,0 +1,14 @@
+package com.dindac.myactionbar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu1);
+    }
+}
